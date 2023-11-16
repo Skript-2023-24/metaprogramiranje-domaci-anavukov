b9d320d079e86f6e1c7e3d70cdc2f1e5c1e7bbb2
anavukov <vukov555@gmail.com>
2023-11-16 21:29:49 +0100

    test primeri u mainu

--- main.rb
+++ main.rb
@@ -7,10 +7,31 @@ ws = session.spreadsheet_by_key("1Gu1zzZicp2FHL_hcc3pIPCt70LhtZyqbwls0C5qgdNY").
 tabela = GoogleSheetEnumerable.new(ws)
 
 while true
-    puts "Unesite izraz"
+    puts "Unesite izraz: "
     trenIzraz = gets.chomp
     if trenIzraz == "kraj"
         break
     end
     puts instance_eval trenIzraz
 end
+#test primeri
+
+# tabela.each do |value, row, col|
+#   puts "Red #{row}, Kolona #{col}: #{value}"
+# end
+# tabela['Prva kolona'][2] = 15
+# incremented_values = tabela.PrvaKolona.map do |cell|
+#   cell_value = cell.to_i  # Convert cell value to integer
+#   cell_value + 1  # Increment the value by 1
+# end
+# puts "Original values: #{tabela.PrvaKolona.to_a}"
+# puts "Incremented values: #{incremented_values}"
+
+
+# selected_values = tabela.PrvaKolona.select do |cell|
+#   cell.to_i < 5  # Convert cell value to integer and check if it's greater than 20
+# end
+
+# puts "Selected values smaller than 5: #{selected_values}"
+# puts "Reduce in Prva Kolona: #{tabela.PrvaKolona.reduce(0) { |sum, value| sum + value.to_i }}"
+# puts tabela.Indeks.find_row_by_value('rn1234')
--- ruby.rb
+++ ruby.rb
@@ -147,26 +147,3 @@ class GoogleSheetEnumerable
   end
  
 end
-
-#test primeri
-
-# tabela.each do |value, row, col|
-#   puts "Red #{row}, Kolona #{col}: #{value}"
-# end
-# tabela['Prva kolona'][2] = 15
-
-# incremented_values = tabela.PrvaKolona.map do |cell|
-#   cell_value = cell.to_i  # Convert cell value to integer
-#   cell_value + 1  # Increment the value by 1
-# end
-# puts "Original values: #{tabela.PrvaKolona.to_a}"
-# puts "Incremented values: #{incremented_values}"
-
-
-# selected_values = tabela.PrvaKolona.select do |cell|
-#   cell.to_i < 5  # Convert cell value to integer and check if it's greater than 20
-# end
-
-# puts "Selected values smaller than 5: #{selected_values}"
-# puts "Reduce in Prva Kolona: #{tabela.PrvaKolona.reduce(0) { |sum, value| sum + value.to_i }}"
-# puts tabela.Indeks.find_row_by_value('rn1234')
